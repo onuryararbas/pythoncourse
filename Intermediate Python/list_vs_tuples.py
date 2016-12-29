cc11d745363cc26bb7827300027fd65c3e3ba0a6
onuryararbas <onuryararbas@gmail.com>
2016-12-29 13:12:00 -0800

    lists and tuples

---
+++ Intermediate Python/list_vs_tuples.py
@@ -0,0 +1,14 @@
+def example():
+    return 15,19
+a,b = example()
+
+
+print(a)
+print(b)
+
+
+x = [1,2,3,4,5,6,7,8,9]
+
+print(x)
+print(x[5])
+#the 5 finds the 5th letter in the list and repeats it to you.
