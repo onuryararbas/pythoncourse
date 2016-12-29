3d9eba75a4c1e68924c109ee7f1dba87bbe22461
onuryararbas <onuryararbas@gmail.com>
2016-12-29 13:17:44 -0800

    dictionaries

---
+++ Intermediate Python/dictionaries.py
@@ -0,0 +1,12 @@
+gradeDict = {'Kelly':89, 'David':65, 'Jack':95, 'Samantha':78}
+print(gradeDict)
+
+print(gradeDict['David'])
+
+gradeDict["David"] = 56
+
+print(gradeDict)
+
+gradeDict['Jessy'] = 92
+
+print(gradeDict)
