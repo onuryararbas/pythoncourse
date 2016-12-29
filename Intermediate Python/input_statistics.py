b2d2f18870ac02f52c94b41198b1ede77565dbf0
onuryararbas <onuryararbas@gmail.com>
2016-12-29 12:30:20 -0800

    statistics and input examples

--- Intermediate Python/input_statistics.py
+++ Intermediate Python/input_statistics.py
@@ -1,3 +1,19 @@
 
 name = input('what is your name?:')
 print('Hello', name)
+
+import statistics
+
+List = [1,2,3,4,5,6,7,8,9,10]
+
+x = statistics.mean(List)
+print(x)
+
+x = statistics.median(List)
+print (x)
+
+x = statistics.stdev(List)
+print(x)
+
+x = statistics.variance(List)
+print(x)
